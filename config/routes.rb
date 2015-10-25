9149f1a15b6aebfbdcfcc95706b353a62b2795ff
Drkstr <r.rakshak@gmail.com>
2015-10-25 14:11:58 +1100

    Root the splash screen

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :notes
   resources :patients
   resources :models
+
+  root 'splash#splash'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
